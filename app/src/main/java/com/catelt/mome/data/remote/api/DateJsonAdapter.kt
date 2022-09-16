f5aa784a33b7ad5da1b9e83c9ef000a38f81960d
Hồ Văn Duy <hovanduy0501@gmail.com>
2022-09-16 10:34:10 +0700

    Setup Api Movie

---
+++ app/src/main/java/com/catelt/mome/data/remote/api/DateJsonAdapter.kt
@@ -0,0 +1,54 @@
+package com.catelt.mome.data.remote.api
+
+import android.annotation.SuppressLint
+import com.squareup.moshi.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateJsonAdapter : JsonAdapter<Date>() {
+    private companion object {
+        @SuppressLint("SimpleDateFormat")
+        val serverDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+        @SuppressLint("SimpleDateFormat")
+        val isoDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    }
+
+    @FromJson
+    override fun fromJson(reader: JsonReader): Date? {
+        val dateString = reader.nextString()
+
+        if (dateString.isEmpty()) {
+            return null
+        }
+
+        val serverDateFormat = serverDateFormat.parseOrNull(dateString)
+        if (serverDateFormat != null) {
+            return serverDateFormat
+        }
+
+        val isoDate = isoDateFormat.parseOrNull(dateString)
+        if (isoDate != null) {
+            return isoDate
+        }
+        return null
+    }
+
+    @ToJson
+    override fun toJson(writer: JsonWriter, value: Date?) {
+        if (value != null) {
+            writer.value(value.toString())
+        }
+    }
+}
+
+private fun SimpleDateFormat.parseOrNull(source: String): Date? {
+    return try {
+        parse(source)
+    } catch (e: ParseException) {
+        null
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        null
+    }
+}
---
+++ app/src/main/java/com/catelt/mome/data/remote/api/movie/TmdbMoviesApi.kt
@@ -0,0 +1,35 @@
+package com.catelt.mome.data.remote.api.movie
+
+import androidx.annotation.FloatRange
+import com.catelt.mome.data.model.*
+import com.catelt.mome.data.model.movie.MoviesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbMoviesApi {
+    @GET("configuration")
+    fun getConfig(): Call<Config>
+
+    @GET("discover/movie")
+    suspend fun discoverMovies(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String,
+        @Query("sort_by") type: SortTypeParam,
+        @Query("with_genres") genres: GenresParam,
+        @Query("with_watch_providers") watchProviders: WatchProvidersParam,
+        @Query("watch_region") watchRegion: String = region,
+        @FloatRange(from = 0.0)
+        @Query("vote_average.gte")
+        voteAverageMin: Float,
+        @FloatRange(from = 0.0)
+        @Query("vote_average.lte")
+        voteAverageMax: Float,
+        @Query("release_date.gte")
+        fromReleaseDate: DateParam?,
+        @Query("release_date.lte")
+        toReleaseDate: DateParam?
+    ): MoviesResponse
+}
