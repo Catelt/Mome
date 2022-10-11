6e9055c66a816908187913f9d2d2891659c1a219
Hồ Văn Duy <54297602+Catelt@users.noreply.github.com>
2022-10-11 13:58:46 +0700

    UI Video Play
    Set up Exoplayer

--- app/src/main/java/com/catelt/mome/utils/Constants.kt
+++ app/src/main/java/com/catelt/mome/utils/Constants.kt
@@ -3,3 +3,5 @@ package com.catelt.mome.utils
 const val SIZE_PAGE = 20
 
 const val BUNDLE_ID_MEDIA = "id_media"
+const val BUNDLE_TITLE_MEDIA = "title_media"
+const val BUNDLE_URL_MEDIA = "url_media"
