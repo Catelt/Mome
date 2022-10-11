e64d719c8372ae34f9d1561d614415907cf9e106
Hồ Văn Duy <hovanduy0501@gmail.com>
2022-10-11 10:14:04 +0100

    [Setup] Api tv show (#20)
    * Base API TV Show
    
    * Update build.gradle

---
+++ app/src/main/java/com/catelt/mome/data/remote/api/tvshow/TmdbTvShowsApi.kt
@@ -0,0 +1,146 @@
+package com.catelt.mome.data.remote.api.tvshow
+
+import androidx.annotation.FloatRange
+import com.catelt.mome.data.model.*
+import com.catelt.mome.data.model.tvshow.TvSeasonsResponse
+import com.catelt.mome.data.model.tvshow.TvShowDetails
+import com.catelt.mome.data.model.tvshow.TvShowsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbTvShowsApi {
+    @GET("configuration")
+    fun getConfig(): Call<Config>
+
+    @GET("discover/tv")
+    suspend fun discoverTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String,
+        @Query("sort_by") type: SortTypeParam,
+        @Query("with_genres") genres: GenresParam,
+        @Query("with_watch_providers") watchProviders: WatchProvidersParam,
+        @Query("watch_region") watchRegion: String = region,
+        @FloatRange(from = 0.0)
+        @Query("vote_average.gte")
+        voteAverageMin: Float,
+        @FloatRange(from = 0.0)
+        @Query("vote_average.lte")
+        voteAverageMax: Float,
+        @Query("first_air_date.gte")
+        fromAirDate: DateParam?,
+        @Query("first_air_date.lte")
+        toAirDate: DateParam?
+    ): TvShowsResponse
+
+    @GET("tv/top_rated")
+    suspend fun getTopRatedTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/on_the_air")
+    suspend fun getOnTheAirTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/airing_today")
+    suspend fun getAiringTodayTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/{tv_id}")
+    fun getTvShowDetails(
+        @Path("tv_id") tvShowId: Int,
+        @Query("language") isoCode: String
+    ): Call<TvShowDetails>
+
+    @GET("tv/{tv_id}/similar")
+    suspend fun getSimilarTvShows(
+        @Path("tv_id") tvShowId: Int,
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/{tv_id}/recommendations")
+    suspend fun getTvShowsRecommendations(
+        @Path("tv_id") tvShowId: Int,
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/{tv_id}/season/{season_number}")
+    fun getTvSeasons(
+        @Path("tv_id") tvShowId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Query("language") isoCode: String
+    ): Call<TvSeasonsResponse>
+
+    @GET("trending/tv/week")
+    suspend fun getTrendingTvShows(
+        @Query("page") page: Int,
+        @Query("language") isoCode: String,
+        @Query("region") region: String
+    ): TvShowsResponse
+
+    @GET("tv/{tv_id}/season/{season_number}")
+    fun getSeasonDetails(
+        @Path("tv_id") tvShowId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Query("language") isoCode: String
+    ): Call<SeasonDetails>
+
+    @GET("tv/{tv_id}/season/{season_number}/aggregate_credits")
+    fun getSeasonCredits(
+        @Path("tv_id") tvShowId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Query("language") isoCode: String,
+    ): Call<AggregatedCredits>
+
+    @GET("tv/{tv_id}/images")
+    fun getTvShowImages(
+        @Path("tv_id") tvShowId: Int
+    ): Call<ImagesResponse>
+
+    @GET("tv/{tv_id}/season/{season_number}/episode/{episode_number}/images")
+    fun getEpisodeImages(
+        @Path("tv_id") tvShowId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int
+    ): Call<ImagesResponse>
+
+    @GET("genre/tv/list")
+    fun getTvShowsGenres(
+        @Query("language") isoCode: String
+    ): Call<GenresResponse>
+
+    @GET("tv/{tv_id}/videos")
+    fun getTvShowVideos(
+        @Path("tv_id") tvShowId: Int,
+        @Query("language") isoCode: String
+    ): Call<VideosResponse>
+
+    @GET("tv/{tv_id}/season/{season_number}/videos")
+    fun getSeasonVideos(
+        @Path("tv_id") tvShowId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Query("language") isoCode: String
+    ): Call<VideosResponse>
+
+}
---
+++ app/src/main/java/com/catelt/mome/data/remote/api/tvshow/TmdbTvShowsApiHelperImpl.kt
@@ -0,0 +1,157 @@
+package com.catelt.mome.data.remote.api.tvshow
+
+import com.catelt.mome.data.model.*
+import com.catelt.mome.data.model.tvshow.TvSeasonsResponse
+import com.catelt.mome.data.model.tvshow.TvShowDetails
+import com.catelt.mome.data.model.tvshow.TvShowsResponse
+import retrofit2.Call
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TmdbTvShowsApiHelperImpl @Inject constructor(
+    private val tmdbTvShowsApi: TmdbTvShowsApi
+) : TmdbTvShowsApiHelper {
+    override fun getConfig(): Call<Config> {
+        return tmdbTvShowsApi.getConfig()
+    }
+
+    override suspend fun discoverTvShows(
+        page: Int,
+        isoCode: String,
+        region: String,
+        sortTypeParam: SortTypeParam,
+        genresParam: GenresParam,
+        watchProvidersParam: WatchProvidersParam,
+        voteRange: ClosedFloatingPointRange<Float>,
+        fromAirDate: DateParam?,
+        toAirDate: DateParam?
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.discoverTvShows(
+            page,
+            isoCode,
+            region,
+            sortTypeParam,
+            genresParam,
+            watchProvidersParam,
+            voteAverageMin = voteRange.start,
+            voteAverageMax = voteRange.endInclusive,
+            fromAirDate = fromAirDate,
+            toAirDate = toAirDate
+        )
+    }
+
+    override suspend fun getTopRatedTvShows(
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getTopRatedTvShows(page, isoCode, region)
+    }
+
+    override suspend fun getOnTheAirTvShows(
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getOnTheAirTvShows(page, isoCode, region)
+    }
+
+    override suspend fun getPopularTvShows(
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getPopularTvShows(page, isoCode, region)
+    }
+
+    override suspend fun getAiringTodayTvShows(
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getAiringTodayTvShows(page, isoCode, region)
+    }
+
+    override fun getTvShowDetails(tvShowId: Int, isoCode: String): Call<TvShowDetails> {
+        return tmdbTvShowsApi.getTvShowDetails(tvShowId, isoCode)
+    }
+
+    override suspend fun getSimilarTvShows(
+        tvShowId: Int,
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getSimilarTvShows(tvShowId, page, isoCode, region)
+    }
+
+    override suspend fun getTvShowsRecommendations(
+        tvShowId: Int,
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getTvShowsRecommendations(tvShowId, page, isoCode, region)
+    }
+
+    override fun getTvSeasons(
+        tvShowId: Int,
+        seasonNumber: Int,
+        isoCode: String
+    ): Call<TvSeasonsResponse> {
+        return tmdbTvShowsApi.getTvSeasons(tvShowId, seasonNumber, isoCode)
+    }
+
+    override suspend fun getTrendingTvShows(
+        page: Int,
+        isoCode: String,
+        region: String
+    ): TvShowsResponse {
+        return tmdbTvShowsApi.getTrendingTvShows(page, isoCode, region)
+    }
+
+    override fun getSeasonDetails(
+        tvShowId: Int,
+        seasonNumber: Int,
+        isoCode: String
+    ): Call<SeasonDetails> {
+        return tmdbTvShowsApi.getSeasonDetails(tvShowId, seasonNumber, isoCode)
+    }
+
+    override fun getTvShowImages(tvShowId: Int): Call<ImagesResponse> {
+        return tmdbTvShowsApi.getTvShowImages(tvShowId)
+    }
+
+    override fun getEpisodeImages(
+        tvShowId: Int,
+        seasonNumber: Int,
+        episodeNumber: Int
+    ): Call<ImagesResponse> {
+        return tmdbTvShowsApi.getEpisodeImages(tvShowId, seasonNumber, episodeNumber)
+    }
+
+    override fun getTvShowsGenres(isoCode: String): Call<GenresResponse> {
+        return tmdbTvShowsApi.getTvShowsGenres(isoCode)
+    }
+
+    override fun getTvShowVideos(tvShowId: Int, isoCode: String): Call<VideosResponse> {
+        return tmdbTvShowsApi.getTvShowVideos(tvShowId, isoCode)
+    }
+
+    override fun getSeasonVideos(
+        tvShowId: Int,
+        seasonNumber: Int,
+        isoCode: String
+    ): Call<VideosResponse> {
+        return tmdbTvShowsApi.getSeasonVideos(tvShowId, seasonNumber, isoCode)
+    }
+
+    override fun getSeasonCredits(
+        tvShowId: Int,
+        seasonNumber: Int,
+        isoCode: String
+    ): Call<AggregatedCredits> {
+        return tmdbTvShowsApi.getSeasonCredits(tvShowId, seasonNumber, isoCode)
+    }
+}
