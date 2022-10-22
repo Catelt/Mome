cc43145c8b982214bf57f7e2c703f6b0cf0c1f87
Hồ Văn Duy <hovanduy0504@gmail.com>
2022-10-22 14:11:58 +0700

    Fix format time

--- app/src/main/java/com/catelt/mome/utils/DateFormat.kt
+++ app/src/main/java/com/catelt/mome/utils/DateFormat.kt
@@ -1,11 +1,13 @@
 package com.catelt.mome.utils
 
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 object DateFormat {
-    val month = DateTimeFormatter.ofPattern("MMMM")
-    val monthAverage = DateTimeFormatter.ofPattern("MMM")
-    val upcoming = DateTimeFormatter.ofPattern("MMMM dd")
-    val default = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val localDefault = Locale.ENGLISH
+    val month = DateTimeFormatter.ofPattern("MMMM",localDefault)
+    val monthAverage = DateTimeFormatter.ofPattern("MMM",localDefault)
+    val upcoming = DateTimeFormatter.ofPattern("MMMM dd",localDefault)
+    val default = DateTimeFormatter.ofPattern("yyyy-MM-dd",localDefault)
 }
