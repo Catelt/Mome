5d98b3fba7966f9bbcd06421d6d7055a40a56a62
Hồ Văn Duy <hovanduy0501@gmail.com>
2024-03-28 12:25:11 +0700

    Delete Account Feature (#54)

--- app/src/main/java/com/catelt/mome/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/catelt/mome/ui/profile/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.catelt.mome.ui.profile
 
 import android.content.Intent
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.catelt.mome.R
 import com.catelt.mome.core.BaseFragment
 import com.catelt.mome.databinding.FragmentProfileBinding
 import com.catelt.mome.ui.auth.AuthActivity
@@ -31,6 +33,23 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(
 
             txtLogout.setOnClickListener {
                 viewModel.logOut()
+            }
+
+            txtDeleteAccount.setOnClickListener {
+                context?.let {
+                    val builder = AlertDialog.Builder(it)
+                    builder.setMessage(getString(R.string.delete_account_content))
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                            viewModel.deleteAccount()
+                        }
+                        .setNegativeButton(getString(R.string.no)) { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                    val alert = builder.create()
+                    alert.show()
+                }
+
             }
         }
     }
